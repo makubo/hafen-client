36c49d2d4bf4f87c00ae585d273f5e1eedddbeb8
Ender Wiggin <andrii.gook@gmail.com>
2024-07-14 18:11:26 +0300

    slightly improved color settings panel's layout

--- src/me/ender/OptColorPanel.java
+++ src/me/ender/OptColorPanel.java
@@ -7,9 +7,11 @@ import static haven.OptWnd.*;
 public class OptColorPanel {
     public static void init(OptWnd wnd, OptWnd.Panel panel) {
 	int STEP = UI.scale(25);
+	int H_STEP = UI.scale(10);
 	int START;
 	int x, y;
 	int my = 0, tx;
+	Widget w;
 	
 	Widget title = panel.add(new Label("Color settings", LBL_FNT), 0, 0);
 	START = title.sz.y + UI.scale(10);
@@ -18,50 +20,56 @@ public class OptColorPanel {
 	y = START;
 	panel.add(new CFGColorBtn(CFG.COLOR_MINE_SUPPORT_OVERLAY, "Mine support overlay", false), x, y);
 	
-	y+= STEP;
+	y += STEP;
 	panel.add(new CFGColorBtn(CFG.COLOR_TILE_GRID, "Tile grid", true), x, y);
 	
-	y+= STEP;
-	panel.add(new CFGColorBtn(CFG.COLOR_HBOX_SOLID, "Hit box: Solid", true), x, y);
+	y += STEP;
+	panel.add(new Label("Hit Box:"), x, y);
 	
-	y+= STEP;
-	panel.add(new CFGColorBtn(CFG.COLOR_HBOX_PASSABLE, "Hit box: Passable", true), x, y);
+	y += STEP;
+	tx = H_STEP;
+	tx += panel.add(new CFGColorBtn(CFG.COLOR_HBOX_SOLID, "Solid", true), tx, y).sz.x;
+	panel.add(new CFGColorBtn(CFG.COLOR_HBOX_PASSABLE, "Passable", true), tx + H_STEP, y);
 	
 	my = Math.max(my, y);
 	
-	x += UI.scale(250);
+	x += UI.scale(200);
 	y = START;
 	
 	panel.add(new CFGColorBtn(CFG.COLOR_GOB_READY, "Workstation ready", true), x, y);
 	
-	y+= STEP;
+	y += STEP;
 	panel.add(new CFGColorBtn(CFG.COLOR_GOB_FULL, "Container: Full", true), x, y);
 	
-	y+= STEP;
+	y += STEP;
 	panel.add(new CFGColorBtn(CFG.COLOR_GOB_EMPTY, "Container: Empty", true), x, y);
 	
-	y+= STEP;
+	y += STEP;
 	
 	//combat
-	y+= STEP;
-	panel.add(new CFGColorBtn(CFG.COLOR_GOB_SELF, "Combat highlight: Self", true), x, y);
+	panel.add(new Label("Combat highlights:"), x, y);
 	
-	y+= STEP;
-	panel.add(new CFGColorBtn(CFG.COLOR_GOB_LEADER, "Combat highlight: Leader", true), x, y);
+	tx = x + H_STEP;
+	y += STEP;
+	panel.add(new CFGColorBtn(CFG.COLOR_GOB_SELF, "Self", true), tx, y);
 	
-	y+= STEP;
-	panel.add(new CFGColorBtn(CFG.COLOR_GOB_PARTY, "Combat highlight: Party", true), x, y);
+	y += STEP;
+	panel.add(new CFGColorBtn(CFG.COLOR_GOB_LEADER, "Leader", true), tx, y);
 	
-	y+= STEP;
-	panel.add(new CFGColorBtn(CFG.COLOR_GOB_IN_COMBAT, "Combat highlight: Enemy", true), x, y);
+	y += STEP;
+	panel.add(new CFGColorBtn(CFG.COLOR_GOB_PARTY, "Party", true), tx, y);
 	
-	y+= STEP;
-	panel.add(new CFGColorBtn(CFG.COLOR_GOB_COMBAT_TARGET, "Combat highlight: Current target", true), x, y);
+	y += STEP;
+	panel.add(new CFGColorBtn(CFG.COLOR_GOB_IN_COMBAT, "Enemy", true), tx, y);
+	
+	y += STEP;
+	panel.add(new CFGColorBtn(CFG.COLOR_GOB_COMBAT_TARGET, "Current target", true), tx, y);
 	
 	my = Math.max(my, y);
 	
-	panel.add(wnd.new PButton(UI.scale(200), "Back", 27, wnd.main), new Coord(0, my + UI.scale(35)));
+	w = panel.add(wnd.new PButton(UI.scale(200), "Back", 27, wnd.main), new Coord(0, my + UI.scale(35)));
 	panel.pack();
 	title.c.x = (panel.sz.x - title.sz.x) / 2;
+	w.c.x = (panel.sz.x - w.sz.x) / 2;
     }
 }
